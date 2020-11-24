3eec200377af343bc4a2705bdc3118f76b5f3a1e
K M <mat20014@byui.edu>
2020-11-24 14:17:49 -0700

    Made the working console app

--- rock-paper-sci.py
+++ rock-paper-sci.py
@@ -1,1 +1,36 @@
-print("Welcome to this game\n")
+from random import randint
+
+print("Welcome to this game of Rock, Paper, Scissors!\n")
+
+t = ["Rock", "Paper", "Scissors"]
+computer = t[randint(0,2)]
+
+again = "y"
+count = 0
+
+while again == "y" or again == "Y":
+    value = input("Please enter 'Rock', 'Paper', or 'Scissors' ")
+    if value == computer:
+        print("Tie!")
+    elif value == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", value)
+        else:
+            print("You win!", value, "smashes", computer)
+    elif value == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", value)
+        else:
+            print("You win!", value, "covers", computer)
+    elif value == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", value)
+        else:
+            print("You win!", value, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+    count += 1
+    print(count)
+    if count%3 == 0:
+        again = input("Do you want to play again? (y/n) ")
+    computer = t[randint(0,2)]
